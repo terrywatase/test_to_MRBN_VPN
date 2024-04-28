f4cc2e9441332c846eb779dbaf6054a55c4804e9
terrywatase <terrywatase@gmail.com>
2024-04-28 10:04:30 +0900

    2nd commit

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
-st.title("Filemanagement"
-      )
+import os
+
+st.title("Filemanagement")
+
+folder_path ="\\\\inet\\PUB\\D2320_ディオネカンパニー\\D2321_ディオネカンパニー_HT課"
+
+try: 
+      file_names = os.listdir(folder_path)
+      st.write(file_names) 
+except:
+      st.markdown("""
+                  ### 社外アクセスの場合、VPNが必要です
+                   """)
+
+
